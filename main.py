fac23e1e50da643438fa8cc17f0c884bc7c6b352
Arkaprabha Banerjee <arkaofficial13@gmail.com>
2024-03-25 22:38:29 +0530

    prepare base model done

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 
 from Chest_cancer import logger
 from Chest_cancer.pipeline.stage_01_Data_ingestion import DataIngestionTrainingPipeline
-
+from Chest_cancer.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 try:
@@ -18,3 +18,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
