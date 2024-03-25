2751080e48dff20c23f9ef411e412aa1612c2de4
Arkaprabha Banerjee <arkaofficial13@gmail.com>
2024-03-25 22:02:44 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from src.Chest_cancer import logger
+# from src.Chest_cancer import logger
+# from Chest_cancer import logger
+
+
+# logger.info("Welcome to Chest cancer detection")
+
+
 from Chest_cancer import logger
+from Chest_cancer.pipeline.stage_01_Data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("Welcome to Chest cancer detection")
-
+STAGE_NAME="Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
